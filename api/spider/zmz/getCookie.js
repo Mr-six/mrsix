9a7aaf512183870b9ea646226f8dd543cdfb1d77
mr-six <582497914@qq.com>
2017-10-25 23:03:04 +0800

    debug zmz api

--- api/spider/zmz/getCookie.js
+++ api/spider/zmz/getCookie.js
@@ -1,14 +1,17 @@
 const axios  = require('axios')
 const config = require('../config')
 const $      = require('../../../utils')
-// const redis  = require('../../../utils/redis')
+const redis  = require('../../../utils/redis')
 const querystring = require('querystring')
 
+axios.defaults.withCredentials = true
+
 let headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+// 正则过滤cookie规则
+const reg_cookie = /GINFO=uid|GKEY=\w{10,}/
 
-Object.assign(headers, config.header)
+Object.assign(headers, config.headers)
 
-// $.info(headers)
 
 let user = {
   account: '582497915@qq.com',
@@ -18,21 +21,46 @@ let user = {
 
 async function getCookie (user) {
   let userStr = querystring.stringify(user)
-  let Cookie
-  let Cookie = await redis.getVal(user.account)
-  if (Cookie) {
-    let {data} = await axios.post(config.zmz.check_login, {headers, Cookie})
-    $.debug(data)
-    if (data.status === 1) return Cookie
+  try {
+    let Cookie = await redis.getVal(user.account)
+    if (Cookie) {
+      $.debug('Cookie', Cookie)
+      Object.assign(headers, {Cookie})
+      $.info('header----', headers)
+      let d = await axios.post(config.zmz.check_login, {headers})
+      $.debug('data', d)
+      if (d.data.status === 1) return Cookie
+    }
+    // $.debug(userStr)
+    let res = await axios.post(config.zmz.sigin_path, userStr, {headers})
+    $.debug(res)
+    if (res.data.status !== 1) return false
+    Cookie =  filterCookie(res.headers['set-cookie']).join(';')
+    // Cookie =  res.headers['set-cookie'].join(';')
+    $.info(Cookie)
+    await redis.setKey(user.account, Cookie)
+    return Cookie
+  } catch (e) {
+    $.error(e)
+    return e
   }
-  $.debug(userStr)
-  let res = await axios.post(config.zmz.sigin_path, userStr, {headers})
-  $.debug(res)
-  if (res.data.status !== 1) return false
-  Cookie = res.headers['set-cookie']
-  $.info(Cookie)
-  await redis.setKey(user.account, Cookie)
-  return Cookie
+  
 }
 
 getCookie(user)
+  .then((res) => $.info(res))
+
+
+function filterCookie (cookie) {
+  let COOKIE = []
+  cookie.forEach(function (el, i) {
+    el.split(';').forEach(function (el, i) {
+      if (el.match(reg_cookie)) {
+        COOKIE.push(el)
+      } else {
+        return false
+      }
+    })
+  })
+  return COOKIE
+}
